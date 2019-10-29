9e55692cc229741e5b23315d3532d450517a3925
thelostbo <2757095747@qq.com>
2019-10-29 17:53:09 +0800

    InsertAds
    Insert advertisements plug-ins.Accomplish a demo.

---
+++ AdsTestProject/Assets/scripts/BtnClick.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Advertisements;
+
+public class BtnClick : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject playBtn = GameObject.Find("Canvas/Plane/Play");
+        GameObject skipBtn = GameObject.Find("Canvas/Plane/Skip");
+        Button play = (Button) playBtn.GetComponent<Button>();
+        Button skip = (Button) skipBtn.GetComponent<Button>();
+        play.onClick.AddListener(()=> {
+            OnClick(playBtn);
+            OnShowAds();
+        });
+        skip.onClick.AddListener(()=>{
+            OnClick(skipBtn);
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnClick(Object bt)
+    {
+        Debug.Log(bt.name);
+    }
+    public void OnShowAds()
+    {
+        if (Advertisement.IsReady())
+        {
+            Advertisement.Show();
+        }
+    }
+}
